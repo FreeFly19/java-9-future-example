9e81db8c4b5237dbe9faddd49d049e7639a30203
oleksandrm <oleksandrm@interlink-ua.com>
2017-12-23 19:11:47 +0200

    Threads #3 - sequence fixed

--- src/main/com/example/Main.java
+++ src/main/com/example/Main.java
@@ -11,23 +11,25 @@ public class Main {
 
         withTimeMetric("Pot word", () -> {
             String[] words = "hello world it is my first code with java nine".split(" ");
+            String[] translatedWords = new String[words.length];
             Thread[] threads = new Thread[words.length];
 
             for (int i = 0; i < words.length; i++) {
                 int n = i;
                 threads[i] = new Thread(() -> {
-                    System.out.println(linguaLeoService.getTranslations(words[n]).get(0));
+                    translatedWords[n] = linguaLeoService.getTranslations(words[n]).get(0);
                 });
                 threads[i].start();
             }
 
-            for (Thread thread : threads) {
+            for (int i = 0; i < threads.length; i++) {
                 try {
-                    thread.join();
+                    threads[i].join();
+                    System.out.println(translatedWords[i]);
                 } catch (InterruptedException e) { /*will never happen, I hope so*/ }
             }
-
         });
+
 
 
     }
