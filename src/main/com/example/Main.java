d193c23510d9e6e653b2c913a308b15f77c8aa16
oleksandrm <oleksandrm@interlink-ua.com>
2017-12-23 19:11:07 +0200

    Threads #2 - time metrics fixed

--- src/main/com/example/Main.java
+++ src/main/com/example/Main.java
@@ -11,11 +11,20 @@ public class Main {
 
         withTimeMetric("Pot word", () -> {
             String[] words = "hello world it is my first code with java nine".split(" ");
+            Thread[] threads = new Thread[words.length];
 
-            for (String word : words) {
-                new Thread(() -> {
-                    System.out.println(linguaLeoService.getTranslations(word).get(0));
-                }).start();
+            for (int i = 0; i < words.length; i++) {
+                int n = i;
+                threads[i] = new Thread(() -> {
+                    System.out.println(linguaLeoService.getTranslations(words[n]).get(0));
+                });
+                threads[i].start();
+            }
+
+            for (Thread thread : threads) {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) { /*will never happen, I hope so*/ }
             }
 
         });
