cd39fcfcba1c2405312c6d50efa6ae4ec4e4c154
oleksandrm <oleksandrm@interlink-ua.com>
2017-12-23 19:46:26 +0200

    FutureTask #1 fail

--- src/main/com/example/Main.java
+++ src/main/com/example/Main.java
@@ -3,6 +3,7 @@ package com.example;
 import ligualeo.LinguaLeoService;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 
 public class Main {
 
@@ -13,14 +14,14 @@ public class Main {
 
         withTimeMetric("Pot word", () -> {
             String[] words = "hello world it is my first code with java nine".split(" ");
-            MyFuture<String>[] futures = new MyFuture[words.length];
+            FutureTask<String>[] futures = new FutureTask[words.length];
 
             for (int i = 0; i < futures.length; i++) {
                 int n = i;
-                futures[i] = new MyFuture<>(() -> linguaLeoService.getTranslations(words[n]).get(0));
+                futures[i] = new FutureTask<>(() -> linguaLeoService.getTranslations(words[n]).get(0));
             }
 
-            for (MyFuture<String> future : futures) {
+            for (FutureTask<String> future : futures) {
                 try {
                     System.out.println(future.get());
                 } catch (Exception e) { /* As a client, I believe that LinguaLeo and mclout will never fail */ }
@@ -29,38 +30,6 @@ public class Main {
 
 
 
-    }
-
-    static class MyFuture<T> {
-        private final Thread thread;
-
-        private T result;
-        private Exception exception;
-
-        public MyFuture(Callable<T> callable) {
-            thread = new Thread(() -> {
-                try {
-                    setResult(callable.call());
-                } catch (Exception e) {
-                    setException(e);
-                }
-            });
-            thread.start();
-        }
-
-        public T get() throws Exception {
-            thread.join();
-            if (exception != null) throw exception;
-            return result;
-        }
-
-        private void setResult(T res) {
-            this.result = res;
-        }
-
-        private void setException(Exception exception) {
-            this.exception = exception;
-        }
     }
 
 
